efb7c8addbf5dd3e2cc538eed341c559b43455cd
AgutuSam <sammyhardcell@gmail.com>
2021-07-13 12:29:53 +0300

    text_editor

---
+++ lib/toolbar.dart
@@ -0,0 +1,43 @@
+import 'package:community_material_icon/community_material_icon.dart';
+import 'package:flutter/material.dart';
+
+import 'text_field.dart';
+
+class Toolbar extends StatelessWidget {
+  const Toolbar({this.onSelected, this.selectedType, Key? key})
+      : super(key: key);
+
+  final selectedType;
+  final onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(56),
+      child: Material(
+          elevation: 4.0,
+          color: Colors.orange.shade50,
+          shadowColor: Colors.orangeAccent,
+          child: Row(children: <Widget>[
+            IconButton(
+                icon: Icon(CommunityMaterialIcons.format_size,
+                    color: selectedType == SmartTextType.H1
+                        ? Colors.teal
+                        : Colors.black),
+                onPressed: () => onSelected(SmartTextType.H1)),
+            IconButton(
+                icon: Icon(CommunityMaterialIcons.format_quote_open,
+                    color: selectedType == SmartTextType.QUOTE
+                        ? Colors.teal
+                        : Colors.black),
+                onPressed: () => onSelected(SmartTextType.QUOTE)),
+            IconButton(
+                icon: Icon(CommunityMaterialIcons.format_list_bulleted,
+                    color: selectedType == SmartTextType.BULLET
+                        ? Colors.teal
+                        : Colors.black),
+                onPressed: () => onSelected(SmartTextType.BULLET))
+          ])),
+    );
+  }
+}
